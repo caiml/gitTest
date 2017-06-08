43227eebb0ed1191a7d321ce185fd1eba78fcb9a
caiml <caimemory.gmail.com>
2017-06-08 18:34:11 +0800

    git tracks changes

--- main/main.js
+++ main/main.js
@@ -1,4 +1,7 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+    function deplex(x,y){
+		var z=x%y;
+		return (z);
+	}
+	console.log(deplex(x,y));
 };
